5de26976ff20b8e86e6d22b3a33403939098354e
a.rashkov <a.rashkov@astoundcommerce.com>
2023-05-26 21:31:10 +0300

    Adds main navigation

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,5 +1,41 @@
+import { NavLink } from "react-router-dom"
+
+import classes from './MainNavigation.module.css';
+
 const MainNavigation = () => {
-    return <div>Main nav</div>
+    return (
+        <header className={classes.header}>
+            <nav>
+                <ul className={classes.list}>
+                    <li>
+                        <NavLink
+                            to='/'
+                            className={({ isActive }) => isActive ? classes.active : undefined}
+                            end                           
+                        >
+                            Events
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/new-event'
+                            className={({ isActive }) => isActive ? classes.active : undefined}
+                        >
+                            Create Event
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to='/wishlist'
+                            className={({ isActive }) => isActive ? classes.active : undefined}
+                        >
+                            Wish List
+                        </NavLink>
+                    </li>                    
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default MainNavigation;
