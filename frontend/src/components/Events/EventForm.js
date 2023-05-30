b0b7170bf329e2fdca222a0dd12956b5867c8dd1
a.rashkov <a.rashkov@astoundcommerce.com>
2023-05-30 23:11:09 +0300

    Detects when local storage is update on new event

--- frontend/src/components/Events/EventForm.js
+++ frontend/src/components/Events/EventForm.js
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { addNewEvent } from '../../store/event-actions';
+import { useNavigate } from 'react-router-dom';
 
 import { Formik, Form } from 'formik';
 import validationSchema from '../../validators/validation-schema';
@@ -11,24 +12,41 @@ import Loader from '../UI/Loader';
 
 import classes from './EventForm.module.css';
 import categoryNames from '../../utils/category-names';
-import transformEventData from '../../utils/transform-event-data';
+import { transformNewEventData } from '../../utils/transform-event-data';
+import { useEffect, useRef } from 'react';
 
 const nextDay = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
 
 const EventForm = () => {
   const isLoading = useSelector((state) => state.ui.isLoading);
+  const events = useSelector((state) => state.events.events);
+
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const newEventId = useRef('');
 
   const handleFormSubmit = (values, props) => {
-    // should disable submit  button
+    const event = transformNewEventData(values);
 
-    const event = transformEventData(values);
+    newEventId.current = event.id;
 
     dispatch(addNewEvent(event));
-    props.setSubmitting(false);
 
-    // redirect user to events page/home
+    props.setSubmitting(false);
   };
+
+  // Detect when new event is added to Redux
+  useEffect(() => {
+    const isNewEventAdded = events.filter(
+      (event) => event.id === newEventId.current
+    ).length;
+
+    if (isNewEventAdded) {
+      // redirect user to the new event page
+      navigate(`../event/${newEventId.current}`);
+    }
+  }, [events, navigate]);
+
   return (
     <Formik
       initialValues={{
@@ -107,7 +125,9 @@ const EventForm = () => {
           type='number'
           placeholder='e.g 1.25 or leave empty for free event'
         />
-        <button type='submit'>Create Event</button>
+        <button type='submit' disabled={isLoading}>
+          Create Event
+        </button>
       </Form>
     </Formik>
   );
