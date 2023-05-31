80fcce14097d353a9a1d44eee3f682ef5cdaa047
a.rashkov <a.rashkov@astoundcommerce.com>
2023-05-31 12:55:19 +0300

    Fix indentation of wish list components

--- frontend/src/components/WishList/WishListTotals.js
+++ frontend/src/components/WishList/WishListTotals.js
@@ -2,15 +2,23 @@ import { useSelector } from 'react-redux';
 import classes from './WishListTotals.module.css';
 
 const WishListTotals = () => {
-    const totalWishList = useSelector(state => state.events.totalWishList);
-    const totalPrice = useSelector(state => state.events.totalPrice / 100 + ' USD');
+  const totalWishList = useSelector((state) => state.events.totalWishList);
+  const totalPrice = useSelector(
+    (state) => state.events.totalPrice / 100 + ' USD'
+  );
 
-    return (
-        <div className={classes['wishlist-totals']}>
-            <p><span className={classes['wishlist-totals--title']}>Total Items:</span>{totalWishList}</p>
-            <p><span className={classes['wishlist-totals--title']}>Total Price:</span>{totalPrice}</p>
-        </div>
-    );
-}
+  return (
+    <div className={classes['wishlist-totals']}>
+      <p>
+        <span className={classes['wishlist-totals--title']}>Total Items:</span>
+        {totalWishList}
+      </p>
+      <p>
+        <span className={classes['wishlist-totals--title']}>Total Price:</span>
+        {totalPrice}
+      </p>
+    </div>
+  );
+};
 
 export default WishListTotals;
