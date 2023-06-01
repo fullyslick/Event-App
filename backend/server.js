7f138861c38e9ce64287deb3cb71191156242b08
a.rashkov <a.rashkov@astoundcommerce.com>
2023-06-01 14:47:26 +0300

    Adds get-event-details endpoint

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,43 @@ app.get('/api/get-events', (req, res) => {
     } catch (error) {
       res.status(500);
       res.json({
-        error: error,
+        error: error.message,
+      });
+    }
+  });
+});
+
+// Get single event details
+app.get('/api/get-event-details', (req, res, next) => {
+  const eventId = req.query.eventId;
+
+  const url = `${
+    process.env.API_BASE_URL
+  }${process.env.API_SINGLE_EVENT_URL.replace('[eventId]', eventId)}`;
+
+  // Set the Authorization header with the Bearer token
+  const headers = {
+    Authorization: `Bearer ${process.env.API_TOKEN}`,
+  };
+
+  // Make a request to the Eventbrite API
+  request({ url, headers }, async (error, response, body) => {
+    try {
+      if (response.statusCode !== 200) {
+        throw new Error('Unable to fetch data from Eventbrite!');
+      } else {
+        // Transform the data received from the API
+        const transformedData = await transformData.transformEvent(
+          JSON.parse(body)
+        );
+
+        // Send the transformed data back to the client
+        res.json(transformedData);
+      }
+    } catch (error) {
+      res.status(500);
+      res.json({
+        error: error.message,
       });
     }
   });
