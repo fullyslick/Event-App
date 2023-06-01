1077d58c8988d810888bdb4410430f3f453c90c8
a.rashkov <a.rashkov@astoundcommerce.com>
2023-06-01 14:52:33 +0300

    Connects frontend with backend

--- frontend/src/utils/event-api.js
+++ frontend/src/utils/event-api.js
@@ -1,44 +1,13 @@
-import configData from './config.json';
-import { transformApiEvent } from './transform-event-data';
-
-async function transformData(data) {
-  const { events } = data;
-  let eventsData = [];
-
-  try {
-    if (events?.length) {
-      eventsData = events.map((event) => {
-        return transformApiEvent(event);
-      });
-    }
-    return Promise.resolve(eventsData);
-  } catch (error) {
-    return Promise.reject(
-      new Error('Something went wrong while preparing your data!')
-    );
-  }
-}
-
 export async function getEvents() {
   try {
-    const response = await fetch(
-      `https://www.eventbriteapi.com/v3/organizations/${configData.ORGANIZATION_ID}/events/?expand=ticket_availability,venue,category`,
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${configData.API_TOKEN}`,
-        },
-      }
-    );
+    const response = await fetch('http://localhost:8080/api/get-events');
     const data = await response.json();
 
     if (!response.ok) {
       throw new Error('Unable to fetch data!');
     }
 
-    const eventsData = await transformData(data);
-
-    return Promise.resolve(eventsData);
+    return Promise.resolve(data);
   } catch (error) {
     return Promise.reject(error);
   }
@@ -47,13 +16,7 @@ export async function getEvents() {
 export async function getEventDetails(eventId) {
   try {
     const response = await fetch(
-      `https://www.eventbriteapi.com/v3/events/${eventId}/?expand=ticket_availability,venue,category`,
-      {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${configData.API_TOKEN}`,
-        },
-      }
+      `http://localhost:8080/api/get-event-details/?eventId=${eventId}`
     );
     const data = await response.json();
 
@@ -61,9 +24,7 @@ export async function getEventDetails(eventId) {
       throw new Error('Unable to fetch data!');
     }
 
-    const eventData = await transformApiEvent(data);
-
-    return Promise.resolve(eventData);
+    return Promise.resolve(data);
   } catch (error) {
     return Promise.reject(error);
   }
--- frontend/src/utils/transform-event-data.js
+++ frontend/src/utils/transform-event-data.js
@@ -31,27 +31,3 @@ export const transformNewEventData = (inputValues) => {
 
   return event;
 };
-
-export const transformApiEvent = (event) => {
-  const eventObj = {
-    id: event.id,
-    title: event.name.text,
-    description: event.description.text,
-    date: event.start.local,
-    location: event.venue?.name ? event.venue.name : 'Online',
-    address: event.venue?.address?.localized_address_display
-      ? event.venue.address.localized_address_display
-      : '',
-    imageOriginal: event.logo.original.url,
-    image: event.logo.url,
-    category: event.category.name,
-    availableTickets: event.capacity,
-    capacity: event.capacity,
-    price: event.ticket_availability.maximum_ticket_price.value,
-    displayPrice: event.ticket_availability.maximum_ticket_price.major_value,
-    currency: event.ticket_availability.maximum_ticket_price.currency,
-    ticketsWishList: 0,
-  };
-
-  return eventObj;
-};
